0e7d32942427b7c7d77c604e3ea837531421c87a
Akash <akashkumar019@yahoo.com>
2024-04-21 00:46:26 +0530

    feat: add model for transactions

--- moneymanger/transactions/models.py
+++ moneymanger/transactions/models.py
@@ -1,3 +1,42 @@
+from uuid import uuid4
+
 from django.db import models
 
+
 # Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=20)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Transaction(
+    TimeStampedModel
+):  # Transaction Model for storing transaction details in database.
+    class TransactionType(models.TextChoices):
+        INCOME = "Income"
+        EXPENSE = "Expense"
+
+    id = models.UUIDField(primary_key=True, default=uuid4)  # Primary key of the table
+    amount = (
+        models.DecimalField()
+    )  # Amount field to store amount of money involved in a particular transaction.
+    transaction_date = models.DateTimeField()  # Transaction date field to store exact time when a certain transaction took place.
+    transaction_type = models.CharField(
+        max_length=10, choices=TransactionType.choices, default=TransactionType.EXPENSE
+    )
+    note = models.TextField(
+        null=True, blank=True
+    )  # Note field to store any additional notes related to the particular transaction.
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.transaction_type}-{self.category.name}"
