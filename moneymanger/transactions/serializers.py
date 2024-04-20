0cf81506481f38865c386bde8472717d25bea4ee
Akash <akashkumar019@yahoo.com>
2024-04-21 01:21:01 +0530

    feat: add serializer

---
+++ moneymanger/transactions/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Transaction, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'created_at', 'updated_at']
+    
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'amount', 'transaction_date', 'transaction_type', 'note', 'category', 'created_at', 'updated_at']
+
