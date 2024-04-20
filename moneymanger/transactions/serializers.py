9f9f0699bc80cbc7d9ae30eb69de531ffd3d78f9
Akash <akashkumar019@yahoo.com>
2024-04-21 01:23:26 +0530

    chore: perform some cleanup

--- moneymanger/transactions/serializers.py
+++ moneymanger/transactions/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
-from .models import Transaction, Category
+
+from .models import Category, Transaction
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'created_at', 'updated_at']
-    
+        fields = ["id", "name", "description", "created_at", "updated_at"]
+
+
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['id', 'amount', 'transaction_date', 'transaction_type', 'note', 'category', 'created_at', 'updated_at']
-
+        fields = [
+            "id",
+            "amount",
+            "transaction_date",
+            "transaction_type",
+            "note",
+            "category",
+            "created_at",
+            "updated_at",
+        ]
